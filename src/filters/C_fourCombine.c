0031dd467f66dd33b71a634b30ee90df5209c27b
Ignacio Tarrio <itarrio@dc.uba.ar>
2017-04-22 12:10:27 -0300

    C FourCombine

--- src/filters/C_fourCombine.c
+++ src/filters/C_fourCombine.c
@@ -10,6 +10,19 @@
 
 void C_fourCombine(uint8_t* src, uint32_t srcw, uint32_t srch,
                    uint8_t* dst, uint32_t dstw, uint32_t dsth __attribute__((unused))) {
+
+    RGBA (*mSrc)[srcw] = (RGBA (*)[srcw]) src;
+    RGBA (*mDst)[dstw] = (RGBA (*)[dstw]) src;
+    uint32_t halfHeight = srch>>1;
+    uint32_t halfWidth = srcw>>1;
+    for(uint32_t h = 0; h < srch ; h = h + 2) {
+        for(uint32_t w = 0; w < srcw; w = w + 2) {
+            mDst[h][w] = mSrc[h][w];
+            mDst[h][halfWidth + w] = mSrc[h][w + 1];
+            mDst[halfHeight + h][w] = mSrc[h + 1][w];
+            mDst[halfHeight + h][halfHeight + w] = mSrc[h +1][w +1];
+        }
+    }
 }
 
                     
