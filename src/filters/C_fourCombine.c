299c54a722cc6417922fe8827964df8d7cb3648f
Ignacio Tarrio <itarrio@dc.uba.ar>
2017-04-22 13:31:56 -0300

    C FourCombine

--- src/filters/C_fourCombine.c
+++ src/filters/C_fourCombine.c
@@ -10,18 +10,16 @@
 
 void C_fourCombine(uint8_t* src, uint32_t srcw, uint32_t srch,
                    uint8_t* dst, uint32_t dstw, uint32_t dsth __attribute__((unused))) {
-
     RGBA (*mSrc)[srcw] = (RGBA (*)[srcw]) src;
-    RGBA (*mDst)[dstw] = (RGBA (*)[dstw]) src;
+    RGBA (*mDst)[dstw] = (RGBA (*)[dstw]) dst;
     uint32_t halfHeight = srch>>1;
     uint32_t halfWidth = srcw>>1;
-    for(uint32_t h = 0; h < srch ; h = h + 2) {
-        for(uint32_t w = 0; w < srcw; w = w + 2) {
-            mDst[h][w] = mSrc[h][w];
-            mDst[h][halfWidth + w] = mSrc[h][w + 1];
-            mDst[halfHeight + h][w] = mSrc[h + 1][w];
-            mDst[halfHeight + h][halfHeight + w] = mSrc[h +1][w +1];
-        }
+    for(uint32_t h = 0; h < srch ; h++) {
+        for(uint32_t w = 0; w < srcw; w++) {
+            uint32_t offsetH = (h % 2 == 0) ? 0 : halfHeight;
+            uint32_t offsetW = (w % 2 == 0) ? 0 : halfWidth;
+            mDst[offsetH + (h >> 1)][offsetW + (w >> 1)] = mSrc[h][w];
+         }
     }
 }
 
