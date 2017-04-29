c5563af96615054adb0f751e04b11d7d8d0185a9
Ignacio Tarrio <itarrio@dc.uba.ar>
2017-04-29 15:10:40 -0300

    max closer not finished yet

--- src/filters/ASM_maxCloser.asm
+++ src/filters/ASM_maxCloser.asm
@@ -45,33 +45,43 @@ ASM_maxCloser:
 
 			.pintaMax:
 				; TODO: podriamos fijarnos si val es 0 en cuyo caso van los mismos pixeles en toda la imagen
-				xor xmm0, xmm0
+				xor xmm1, xmm1
 				mov r10, kernelOffset
 				.buscarMax:
 											 ; No se si es Valido
-					movdqu xmm1, [rcx + esi * (r11 + r10)  * pixel_size + (r12 - kernelOffset) * pixel_size] ; Agarro los 4 pixeles de la izq de la fila que estamos viendo del kernel
-					pmaxub xmm0, xmm1 ; Me quedo con los mayores
-					movdqu xmm1, [rcx + esi * (r11 + r10)  * pixel_size + r12 * pixel_size] ; Ahora agarramos los de la derecha
-					pmaxub xmm0, xmm1 ; De vuelta nos quedamos con los mayores
+					movdqu xmm2, [rdi + esi * (r11 + r10)  * pixel_size + (r12 - kernelOffset) * pixel_size] ; Agarro los 4 pixeles de la izq de la fila que estamos viendo del kernel
+					pmaxub xmm1, xmm2 ; Me quedo con los mayores
+					movdqu xmm2, [rdi + esi * (r11 + r10)  * pixel_size + r12 * pixel_size] ; Ahora agarramos los de la derecha
+					pmaxub xmm1, xmm2 ; De vuelta nos quedamos con los mayores
 					inc r10
 					cmp r10, kernelMaxOffset
 					jle .buscarMax
 
-				; XMM0 = |Max1|Max2|Max3|Max4|
-				movdqu xmm1, xmm0 
-				psrldq xmm1, 8 ; XMM1 = |0|0|Max1|Max2|
-				pmaxub xmm0, xmm1 ; XMM0 = |Max1|Max2|Max3v1|Max4v2|
-				movdqu xmm1, xmm0 
-				psrldq xmm1, 4 ; XMM1 = |0|Max1|Max2|Max3v1|
-				movdqu xmm0, xmm1 ; XMMP = |?|?|?|MAX|
+				; XMM1 = |Max1|Max2|Max3|Max4|
+				movdqu xmm2, xmm1 
+				psrldq xmm2, 8 ; XMM2 = |0|0|Max1|Max2|
+				pmaxub xmm1, xmm2 ; XMM1 = |Max1|Max2|Max3v1|Max4v2|
+				movdqu xmm2, xmm1 
+				psrldq xmm2, 4 ; XMM2 = |0|Max1|Max2|Max3v1|
+				movdqu xmm1, xmm2 ; XMM1 = |?|?|?|MAX| MAX=|R|G|B|A|
+				
+				pxor xmm15, xmm15
+				punpcklbw xmm1, xmm15 
+				punpcklwd xmm1, xmm15 
+				mulps xmm1, xmm7 ; xmm7 = val
 
+				;???
+				movdqu xmm2, [rdi + esi * r11 * pixel_size + r12 * pixel_size], pixelBlanco ; Pintamos de blanco
+				punpcklbw xmm2, xmm15 
+				punpcklwd xmm2, xmm15 
+				mulps xmm2, xmm6 ; xmm6 = val
 
+				addps xmm2, xmm1
+				
+				cvtps2dq xmm2, xmm2 ;paso a dword
 
-
-
-
-
-
+				;pasar a dword a byte
+				;guardar en memoria
 
 			.aumentarColumna:
 				inc r12 
@@ -81,27 +91,4 @@ ASM_maxCloser:
 			inc r11
 			cmp r11, edx
 			jle .filaLoop
-
-		; if (Fila < 3 || COLUMN < 3 || Fila > Ancho - 4 || COLUMN > Altura - 4) {Pinto Blanco} else {Calculo con el Maximo}
-		cmp r12, 3
-		jl .blanco
-		cmp r11, r15
-		jg .blanco
-		cmp r12, 3
-		jle .calcularMax
-		.blanco:
-			mov dq [rcx + esi * r11 * pixel_size + r12 * pixel_size], blanco
-			jmp 
-		.calcularMax:
-			mov r15, [rdi + esi * r11 * pixel_size + r12 * pixel_size]
-
-
-		.aumentarColumna:
-			add r11, 4
-			jmp .filaLoop 
-
-
-
-
-
 ret
